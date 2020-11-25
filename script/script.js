7d7298540696cf69289a47ad4fe869f6c6ae7317
Golovin Sergey <gsa21076@mail.ru>
2020-11-25 11:56:23 +0300

    дипломный проект

---
+++ script/script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  // popup-call
+  function popupCall() {
+    const callBtn = document.querySelectorAll('a[class=call-btn]'),
+      popupCall = document.querySelector('.popup-call');
+
+    callBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popupCall.style.display = 'block';
+      });
+      popupCall.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target.classList.contains('popup-close')) {
+          popupCall.style.display = 'none';
+        } else {
+          target = target.closest('.popup-dialog');
+          if (!target) {
+            popupCall.style.display = 'none';
+          }
+        }
+      });
+
+    });
+
+
+  }
+
+  popupCall();
+
+
+
+
+});
