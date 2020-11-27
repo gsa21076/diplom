f34ea2301a75fc70258ebef8ecc458600fcfecd1
Golovin Sergey <gsa21076@mail.ru>
2020-11-27 21:41:57 +0300

    fix script.js calculator

--- script/script.js
+++ script/script.js
@@ -170,4 +170,206 @@ window.addEventListener('DOMContentLoaded', () => {
   };
   accordeon();
 
+
+  // calc
+  const calc = () => {
+
+    const accordion = document.getElementById('accordion'),
+      panelDefault = accordion.querySelectorAll('.panel-default'),
+      panelCollapse = accordion.querySelectorAll('.panel-collapse'),
+      boxNumber = document.getElementById('myonoffswitch'),
+      haveButtom = document.getElementById('myonoffswitch-two'),
+      calcResult = document.getElementById('calc-result'),
+      boxTwo = accordion.querySelector('.box-two'),
+      distanse = accordion.querySelector('.distanse'),
+      firstDiam = accordion.querySelector('.first-diam'),
+      firstNumber = accordion.querySelector('.first-number'),
+      secondDiam = accordion.querySelector('.second-diam'),
+      secondNumber = accordion.querySelector('.second-number');
+
+    let customOrder = {
+      numberBox: 1,
+      diam1: 1.4,
+      number1: 1,
+      diam2: 1.4,
+      number2: 1,
+      bottom: false,
+      distanse: 1
+    },
+      sum = 0,
+      k1 = 0,
+      k2 = 0,
+      k3 = 0,
+      k4 = 0;
+
+
+    const oneCamera = 10000,
+      twoCamera = 5000;
+    boxTwo.style.display = 'none';
+
+    const boxChange = () => {
+      if (boxNumber.checked) {
+        boxNumber.checked = false;
+        customOrder.numberBox = 2;
+        boxTwo.style.display = 'block';
+
+      } else {
+        boxNumber.checked = true;
+        customOrder.numberBox = 1;
+        boxTwo.style.display = 'none';
+      }
+
+    };
+
+
+    const boxControlChange = (target) => {
+      if (firstDiam.value === '1.4 метра') {
+        customOrder.diam1 = 1.4;
+        k1 = 0;
+      }
+      if (firstDiam.value === '2 метра') {
+        customOrder.diam1 = 2;
+        k1 = 0.2;
+      }
+      if (firstNumber.value === '1 штука') {
+        customOrder.number1 = 1;
+        k2 = 0;
+      }
+      if (firstNumber.value === '2 штуки') {
+        customOrder.number1 = 2;
+        k2 = 0.3;
+      }
+      if (firstNumber.value === '3 штуки') {
+        customOrder.number1 = 3;
+        k2 = 0.5;
+      }
+      if (secondDiam.value === '1.4 метра') {
+        customOrder.diam2 = 1.4;
+        k3 = 0;
+      }
+      if (secondDiam.value === '2 метра') {
+        customOrder.diam2 = 2;
+        k3 = 0.2;
+      }
+      if (secondNumber.value === '1 штука') {
+        customOrder.number2 = 1;
+        k4 = 0;
+      }
+      if (secondNumber.value === '2 штуки') {
+        customOrder.number2 = 2;
+        k4 = 0.2;
+      }
+      if (secondNumber.value === '3 штуки') {
+        customOrder.number2 = 3;
+        k4 = 0.4;
+      }
+    };
+
+
+    const bottomControlChange = () => {
+      console.log(haveButtom.checked);
+      if (!haveButtom.checked) {
+        haveButtom.checked = true;
+        customOrder.bottom = true;
+      } else {
+        haveButtom.checked = false;
+        customOrder.bottom = false;
+
+      }
+    };
+
+    const distanseChange = () => {
+      distanse.addEventListener('input', () => {
+        customOrder.distanse = distanse.value;
+
+      });
+    };
+
+    const countSum = () => {
+      let sum = 0;
+      if (customOrder.numberBox === 1) {
+        sum = oneCamera + oneCamera * k1 + oneCamera * k2;
+      }
+
+      if (customOrder.numberBox === 2) {
+        sum = oneCamera + oneCamera * k1 + oneCamera * k2 + twoCamera + twoCamera * k3 + twoCamera * k4;
+      }
+
+      if (customOrder.bottom) {
+        if (customOrder.numberBox === 1) {
+          sum = sum + sum * 0.1;
+        } else {
+          sum = sum + sum * 0.2;
+        }
+
+      }
+      calcResult.textContent = sum;
+
+    };
+
+    const togglePanel = (index) => {
+      for (let i = 0; i < panelCollapse.length; i++) {
+        console.log(index, i);
+        if (index === i) {
+          panelCollapse[i].classList.add('in');
+
+        } else {
+          panelCollapse[i].classList.remove('in');
+        }
+      }
+    };
+
+    accordion.addEventListener('click', (event) => {
+      event.preventDefault();
+      let target = event.target;
+      console.log(target);
+      if (target.closest('.panel-one')) {
+        if (target.closest('.onoffswitch')) {
+          boxChange();
+        }
+      }
+      if (target.closest('.panel-two')) {
+        if (target.closest('.form-control')) {
+          boxControlChange(target);
+        }
+      }
+      if (target.closest('.panel-three')) {
+        if (target.closest('.onoffswitch-two')) {
+          bottomControlChange();
+        }
+      }
+      if (target.closest('.panel-four')) {
+        if (target.closest('.distanse')) {
+          distanseChange();
+        }
+      }
+
+      if (target.closest('.construct-btn')) {
+        if (target.closest('.call-btn')) {
+          console.log(customOrder);
+        }
+        panelDefault.forEach((item, i) => {
+          if (item === target.closest('.panel-default')) {
+            i++;
+            togglePanel(i);
+          }
+        });
+      } else {
+        target = target.closest('.panel-default');
+        if (target.classList.contains('panel-default')) {
+          panelDefault.forEach((item, i) => {
+            if (item === target) {
+              togglePanel(i);
+            }
+          });
+        }
+      }
+      countSum();
+    });
+
+  };
+  calc();
+
+
 });
+
