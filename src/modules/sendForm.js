cc1261efe9ef136eb354ed7abf177f6c250d6656
gsa21076 <57519287+gsa21076@users.noreply.github.com>
2020-12-01 13:38:59 +0300

    fix sendForm.js

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,24 +1,6 @@
 const sendForm = () => {
   let customOrder = JSON.parse(sessionStorage.myOrder);
 
-  if (customOrder.useCalc) {
-    customOrder = JSON.parse(sessionStorage.myOrder);
-    sessionStorage.clear();
-  } else {
-    customOrder = {
-      useCalc: false,
-      numberBox: 1,
-      diam1: 1.4,
-      number1: 1,
-      diam2: 1.4,
-      number2: 1,
-      bottom: false,
-      distanse: 0,
-      sum: 10000,
-      question: ''
-    };
-  }
-
   const errorMessage = 'Что-то пошло не так...',
     loadMessage = 'Загрузка...',
     successMessage = 'Спасибо! Мы скоро с Вами свяжемся.';
@@ -26,16 +8,15 @@ const sendForm = () => {
   const form = document.querySelectorAll('form'),
     userName = document.querySelectorAll('input[name=user_name]'),
     userPhone = document.querySelectorAll('.phone-user'),
-    userQuest = document.querySelector('input[name=user_quest]');
+    userQuest = document.querySelector('input[name=user_quest]'),
+    popup = document.querySelectorAll('.popup');
 
   const statusMessage = document.createElement('div');
   statusMessage.style.cssText = `font-size: 1,5rem;
     color: black;`;
 
   const postData = (user) => {
-    userName.forEach(elem => elem.value = '');
-    userPhone.forEach(elem => elem.value = '');
-    userQuest.value = '';
+
     return fetch('./server.php', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -46,13 +27,13 @@ const sendForm = () => {
 
   userPhone.forEach((phone) => {
     phone.addEventListener('input', (event) => {
-      event.target.value = event.target.value.replace(/[^\+\-\d(10)-]/g, '');
+      event.target.value = event.target.value.replace(/^(\+)?([- ()]?\d[- _()]?){12,15}$/, '');
     });
   });
 
   userName.forEach((name) => {
     name.addEventListener('input', (event) => {
-      event.target.value = event.target.value.replace(/[^А-яЁё\ \,\.]/g, '');
+      event.target.value = event.target.value.replace(/[^А-яЁё\ \,\.]/g, '-');
     });
   });
 
@@ -61,14 +42,19 @@ const sendForm = () => {
   });
 
   form.forEach((form) => {
+
     form.addEventListener('submit', (event) => {
       event.preventDefault();
+      customOrder = JSON.parse(sessionStorage.myOrder);
+      form.append(statusMessage);
+      statusMessage.textContent = loadMessage;
       customOrder.question = userQuest.value;
       const formData = new FormData(form);
       let body = {};
       formData.forEach((val, key) => {
         body[key] = val;
       });
+
       let user = Object.assign(body, customOrder);
 
       postData(user)
@@ -83,8 +69,33 @@ const sendForm = () => {
           console.error(error);
         });
       userQuest.required = false;
+
+      popup.forEach((elem) => {
+        setTimeout(() => {
+          elem.style.display = 'none';
+          userName.forEach(elem => elem.value = '');
+          userPhone.forEach(elem => elem.value = '');
+          userQuest.value = '';
+          statusMessage.textContent = '';
+
+        }, 3000);
+      });
     });
+
   });
+  customOrder = {
+    useCalc: false,
+    numberBox: 1,
+    diam1: 1.4,
+    number1: 1,
+    diam2: 1.4,
+    number2: 1,
+    bottom: false,
+    distanse: 0,
+    sum: 10000,
+    question: ''
+  };
+  sessionStorage.myOrder = JSON.stringify(customOrder);
 };
 
 export default sendForm;
